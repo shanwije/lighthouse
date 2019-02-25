128ebebff9e81ac54f3de48c0ff5b0489437fcdd
Matt Zeunert <matt@mostlystatic.com>
2019-02-25 20:43:56 +0000

    Don't add channel to version

--- lighthouse-core/runner.js
+++ lighthouse-core/runner.js
@@ -107,9 +107,7 @@ class Runner {
       }
 
       // Entering: conclusion of the lighthouse result object
-      const lighthousePackageVersion = require('../package.json').version;
-      const lighthouseChannel = runOpts.config.settings.channel;
-      const lighthouseVersion = `${lighthousePackageVersion}+${lighthouseChannel}`;
+      const lighthouseVersion = require('../package.json').version;
 
       /** @type {Object<string, LH.Audit.Result>} */
       const resultsById = {};
